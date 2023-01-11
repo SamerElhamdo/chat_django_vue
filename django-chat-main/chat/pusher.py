939048587e27fd64d12f46f0b58f7ebce368cbbc
samerelhamdo <samer.elhamdoo@gmail.com>
2023-01-12 01:55:18 +0300

    hide secret

--- django-chat-main/chat/pusher.py
+++ django-chat-main/chat/pusher.py
@@ -1,9 +1,11 @@
+import imp
+from .secrets import SECRET
 import pusher
 
 pusher_client = pusher.Pusher(
     app_id = "1536673",
     key = "9e12adb124b797089b79",
-    secret = "e850fe0b361b41e7af25",
+    secret =  SECRET ,
     cluster = "us2",
     ssl=True
 )
